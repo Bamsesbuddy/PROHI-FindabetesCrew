25741616deac795b538bf188cbb7d728b1c64fce
annalarsen25 <anna.b.larsen97@gmail.com>
2025-09-16 14:14:32 +0200

    Added the logo to the questionnaire.
    Also edited some in the text.

--- dashboard_patient/pages/Questionnaire.py
+++ dashboard_patient/pages/Questionnaire.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
+# Sidebar configuration
+st.sidebar.image("./assets/LogoFindabetes.png",)
+st.sidebar.success("Select a tab above.")
+
 st.write("# Questionnaire")
-st.write(" ### Fill out the form below. The answers will be sent to your physician for further ...")
+st.write(" ### Fill out the form below. The answers will be sent to your physician for further assessment.")
 
 option = st.selectbox(
     "By proceeding, you consent to your responses being collected and used for medical purposes in accordance with Findabetes' data handling and privacy policy. Please read more at XXX",
