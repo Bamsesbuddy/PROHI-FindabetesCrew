0b020b00bee725418e5129864d022597e564cce0
annalarsen25 <anna.b.larsen97@gmail.com>
2025-09-18 15:45:38 +0200

    Solved: The issue with the pagelink from Predictive to Prescriptive page
    On the page 3_Predictive_Analytics, I commented the part where the default code from Luis was to load the model. This caused an error since we don't have a trained model yet to load into the code. When we do, we can uncomment this part! (OBS This is my very qualified killgissning)
    
    As for the page links/page redirections: there was just a minor fault in the path. See the code, I kept both the old code (commented, please remove this after you checked)  and the new code.

--- pages/3_Predictive_Analytics.py
+++ pages/3_Predictive_Analytics.py
@@ -11,36 +11,43 @@ st.sidebar.markdown("# Patient prediction 🎯")
 
 st.write("# Example of model prediction")
 
-# Load model
-pre_trained_model_path = "./assets/trained_model.pickle"
-loaded_model = None # This will be replaced by the trained model in the pickle 
+# !!! Commenting this section below until we have the trained model, 
+# to try if the page link code will work :) We will uncomment later.
 
-with open(pre_trained_model_path, "rb") as readFile:
-    loaded_model = pickle.load(readFile)
+# Load model
+#pre_trained_model_path = "./assets/trained_model.pickle"
+#loaded_model = None # This will be replaced by the trained model in the pickle 
+
+#with open(pre_trained_model_path, "rb") as readFile:
+    #loaded_model = pickle.load(readFile)
 
 
 # COLUMNS
-left_column, right_column = st.columns(2)
+#left_column, right_column = st.columns(2)
 
-user_data = []
+#user_data = []
 # Call Streamlit functions inside a "with" block to keep it in a column:
-with left_column:
-    length = st.slider("Sepal Length", min_value=4.0, max_value=9.0, value = 5.0)
-with right_column:
-    width = st.slider("Sepal Width", min_value=1.5, max_value=4.0, value = 3.0)
+#with left_column:
+    #length = st.slider("Sepal Length", min_value=4.0, max_value=9.0, value = 5.0)
+#with right_column:
+    #width = st.slider("Sepal Width", min_value=1.5, max_value=4.0, value = 3.0)
 
-if st.button('Predict!'):
-    user_data = [[length, width]]
-    prediction = loaded_model.predict(user_data)
-    st.write(f"The predicted value for data {user_data} is {prediction}")
+#if st.button('Predict!'):
+    #user_data = [[length, width]]
+    #prediction = loaded_model.predict(user_data)
+    #st.write(f"The predicted value for data {user_data} is {prediction}")
 
 """
 # 
 ⚠️ Add some visualizations to help understanding what the predictions mean...
 """
-
-
+## If we wanted a button that redirects us to Prescriptive Analytics, here it is below..
 if st.button("See detailed view"): 
-    st.switch_page("Dashboard_Clinician/pages/4_Prescriptive_Analytics.py")
+    st.switch_page("pages/4_Prescriptive_Analytics.py")
 
-st.page_link("Dashboard_Clinician/pages/4_Prescriptive_Analytics.py")
+## And if we want just the link to the page, here it is below..
+st.page_link("pages/4_Prescriptive_Analytics.py")
+
+## This was the old one, can be removed, but good to see the difference on the path!
+# st.button("See detailed view"): 
+    #st.switch_page("Dashboard_Clinician/pages/4_Prescriptive_Analytics.py")
---
+++ pages/4_Prescriptive_Analytics.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.markdown("# Patient prescription 🎯")
+
+## insert triage pictures!
