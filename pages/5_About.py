c06f5e184920ae3a598528d10468378885a4ff34
annalarsen25 <anna.b.larsen97@gmail.com>
2025-09-29 13:00:23 +0200

    Minor changes to the text on the about page.

--- pages/5_About.py
+++ pages/5_About.py
@@ -6,7 +6,7 @@ st.markdown(" ## Information about the dataset")
 """This application is based on the dataset from the U.S. Centers for Disease Control and Prevention. Click on this link to read more about the dataset: CDC Diabetes Health Indicators https://archive.ics.uci.edu/dataset/891/cdc+diabetes+health+indicators
 .
 
-We have developed a machine learning model using our own algorithm to predict whether a patient may be at risk for diabetes. The model has been evaluated on real-world data and currently achieves an accuracy of X %, with strong performance across other metrics such as precision (80%) and recall (78%).
+We have developed a machine learning model using our own algorithm to predict whether a patient may be at risk for diabetes. The model has been evaluated on real-world data and currently achieves an accuracy of X %, with strong performance across other metrics such as precision (X %) and recall (X %).
 
 The goal of this tool is to support both patients and physicians in identifying potential risks early and guiding further clinical actions."""
 
