42d0c75cfb27982825677b3cbcdaa0037eaf4c4d
Anton Altmeyer <anton.altm@outlook.de>
2025-10-08 21:29:12 +0200

    added counterfacutal analysis

--- pages/4_Prescriptive_Analytics.py
+++ pages/4_Prescriptive_Analytics.py
@@ -4,6 +4,8 @@ import shap
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.special import expit
+import numpy as np
+from pages.helper import make_donut
 
 st.set_page_config(layout="wide")
 
@@ -42,23 +44,47 @@ base_logit = explainer.expected_value
 shap_sum_logit = base_logit + shap_expl.values.sum()
 shap_prob = expit(shap_sum_logit)
 
+st.header('Patient-Level Insights')
+
 col1, col2 = st.columns(2)
 # ---- Global summary plot (beeswarm) ----
 with col1:
+
     fig = plt.figure(figsize=(8, 4))
-    # using new API plotting helper: beeswarm is a matplotlib plot
-    shap.plots.beeswarm(shap_expl[:, :])   # or shap.plots.bar(shap_expl) for aggregated importance
+    shap.plots.bar(shap_expl) 
     plt.tight_layout()
     st.pyplot(fig)
 
 with col2: 
-    # ---- Local explanation for instance 0: waterfall ----
-    i = 0  # pick an instance index; change as needed or loop
     fig = plt.figure(figsize=(8, 4))
-    shap.plots.waterfall(shap_expl[i, :])   # waterfall works well for one instance
+    shap.plots.waterfall(shap_expl[0])
     plt.tight_layout()
     st.pyplot(fig)
 
+st.subheader("🧠 Counterfactual What-If Explorer")
+st.write("Adjust the top 5 most important features and observe how the model’s prediction changes.")
+
+shap_importance = np.abs(shap_expl.values).mean(axis=0)
+top_features = pd.Series(shap_importance, index=X.columns).sort_values(ascending=False).head(5)
+top_features_list = top_features.index.tolist()
+
+col1, col2 = st.columns(2)
+with col1:
+    for i, feature in enumerate(top_features_list):
+        if feature in ['High BP', 'Smoker','Stroke','Heart Disease or Attack','Physical Activity','Fruits','Veggies','Heavy Alcohol Consumption', 'Difficulties Walking']:
+            default_value = 'Yes' if int(X[feature]) > 0.5 else 'No'
+            value = st.select_slider(label=f'{feature} - Yes or No: ', options=['Yes', 'No'], value=default_value)
+            X[feature] = 1 if value == 'Yes' else 0
+        elif feature in ['BMI', 'General Health', 'Mental Health']:
+            X[feature] = st.number_input(label=f'Select {feature}: ', value=X[feature])
+        elif feature in ['Age']:
+            X[feature] = st.slider(label=f'Select {feature}: ', min_value=0, max_value=100, value=int(X[feature]))
+
+pred_prob = model.predict_proba(X)[0, 1]
+donut_class_one = make_donut(int(pred_prob * 100), 'Outbound Migration', 'red')
+
+with col2:
+    st.altair_chart(donut_class_one)
 
 ### Link to giudelines (recommendations outside of scope)
 
