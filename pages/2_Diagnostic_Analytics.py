044b54a457bb64a81386d9a806ad37e6b6635f96
Anton Altmeyer <anton.altm@outlook.de>
2025-10-13 16:47:47 +0200

    removed the tSNE and UMAP clustering

--- pages/2_Diagnostic_Analytics.py
+++ pages/2_Diagnostic_Analytics.py
@@ -4,8 +4,6 @@ from sklearn.model_selection import train_test_split
 import plotly.express as px
 import plotly.graph_objs as go
 from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
-import umap
 from sklearn.preprocessing import StandardScaler
 
 st.set_page_config(layout="wide")
@@ -57,7 +55,6 @@ else:
 
 
 # ---- PCA cluster Analysis ----
-
 st.subheader("Interactive Cluster Analysis")
 
 sample_frac = 0.05  # fraction of data you want to sample
@@ -93,8 +90,8 @@ with col1:
         x=df_0['PC1'],
         y=df_0['PC2'],
         mode='markers',
-        name='Label 0',
-        marker=dict(size=10, color='blue', symbol='circle'),
+        name='No Diabetes',
+        marker=dict(size=5, color="#4682b4", symbol='circle'),
         text=df_0.index  # optional: hover text
     )
 
@@ -102,26 +99,25 @@ with col1:
         x=df_1['PC1'],
         y=df_1['PC2'],
         mode='markers',
-        name='Label 1',
-        marker=dict(size=10, color='red', symbol='diamond'),
+        name='Diabetes',
+        marker=dict(size=5, color="#B22222", symbol='diamond'),
         text=df_1.index
     )
 
     fig = go.Figure(data=[trace0, trace1])
 
     fig.update_layout(
-        title='PCA Scatter Plot with Graph Objects',
+        title='PCA Scatter Plot',
         xaxis_title='PC1',
         yaxis_title='PC2',
         width=800,
         height=600,
-        legend_title_text='Binary Label'
+        legend_title_text='Absence/Presence Diabetes'
     )
 
     st.plotly_chart(fig)
 
 # ---- 3D PCA ----
-
 pca = PCA(n_components=3)  # reduce to 2 dimensions for plotting
 X_pca = pca.fit_transform(X_scaled)
 
@@ -139,8 +135,8 @@ with col2:
         y=df_0['PC2'],
         z=df_0['PC3'],
         mode='markers',
-        name='Label 0',
-        marker=dict(size=5),
+        name='No Diabetes',
+        marker=dict(size=3, color="#4682b4", symbol='diamond'),
         text=df_0.index,  # optional hover info
         hovertemplate='Index: %{text}<br>PC1: %{x:.2f}<br>PC2: %{y:.2f}<br>PC3: %{z:.2f}<extra></extra>'
     )
@@ -150,8 +146,8 @@ with col2:
         y=df_1['PC2'],
         z=df_1['PC3'],
         mode='markers',
-        name='Label 1',
-        marker=dict(size=5),
+        name='Diabetes',
+        marker=dict(size=3, color="#B22222", symbol='diamond'),
         text=df_1.index,
         hovertemplate='Index: %{text}<br>PC1: %{x:.2f}<br>PC2: %{y:.2f}<br>PC3: %{z:.2f}<extra></extra>'
     )
@@ -167,150 +163,7 @@ with col2:
         ),
         width=900,
         height=700,
-        legend_title_text='Binary Label'
+        legend_title_text='Absence/Presence Diabetes'
     )
 
     st.plotly_chart(fig)
-
-
-# ----- tSNE and UMAP Cluster Analysis
-
-tsne = TSNE(n_components=2, perplexity=30, random_state=42)
-X_tsne = tsne.fit_transform(df_sampled)
-
-# Put PCA result into a DataFrame
-df_tsne = pd.DataFrame(X_tsne, columns=['tSNE1', 'tSNE2'])
-df_tsne['label'] = df_sampled['Diabetes_binary'].values
-
-# Separate data by label
-df_0 = df_tsne[df_tsne['label'] == 0]
-df_1 = df_tsne[df_tsne['label'] == 1]
-
-trace0 = go.Scatter(
-    x=df_0['tSNE1'],
-    y=df_0['tSNE2'],
-    mode='markers',
-    name='Label 0',
-    marker=dict(size=10, color='blue', symbol='circle'),
-    text=df_0.index  # optional: hover text
-)
-
-trace1 = go.Scatter(
-    x=df_1['tSNE1'],
-    y=df_1['tSNE2'],
-    mode='markers',
-    name='Label 1',
-    marker=dict(size=10, color='red', symbol='diamond'),
-    text=df_1.index
-)
-
-fig = go.Figure(data=[trace0, trace1])
-
-fig.update_layout(
-    title='tSNE Scatter Plot',
-    xaxis_title='tSNE1',
-    yaxis_title='tSNE2',
-    width=800,
-    height=600,
-    legend_title_text='Binary Label'
-)
-
-st.plotly_chart(fig)
-
-
-
-
-
-# UMAP
-reducer = umap.UMAP(n_components=2, n_neighbors=15, min_dist=0.1, random_state=42)
-X_umap = reducer.fit_transform(df_sampled)
-
-# Put PCA result into a DataFrame
-df_umap = pd.DataFrame(X_umap, columns=['umap1', 'umap2'])
-df_umap['label'] = df_sampled['Diabetes_binary'].values
-
-# Separate data by label
-df_0 = df_umap[df_umap['label'] == 0]
-df_1 = df_umap[df_umap['label'] == 1]
-
-trace0 = go.Scatter(
-    x=df_0['umap1'],
-    y=df_0['umap2'],
-    mode='markers',
-    name='Label 0',
-    marker=dict(size=10, symbol='circle'),
-    text=df_0.index  # optional: hover text
-)
-
-trace1 = go.Scatter(
-    x=df_1['umap1'],
-    y=df_1['umap2'],
-    mode='markers',
-    name='Label 1',
-    marker=dict(size=10, symbol='diamond'),
-    text=df_1.index
-)
-
-fig = go.Figure(data=[trace0, trace1])
-
-fig.update_layout(
-    title='UMAP Scatter Plot',
-    xaxis_title='UMAP1',
-    yaxis_title='UMAP2',
-    width=800,
-    height=600,
-    legend_title_text='Binary Label'
-)
-
-st.plotly_chart(fig)
-
-
-
-
-
-reducer = umap.UMAP(n_components=3, n_neighbors=15, min_dist=0.1, random_state=42)
-X_umap = reducer.fit_transform(df_sampled)
-
-# Put PCA result into a DataFrame
-df_umap = pd.DataFrame(X_umap, columns=['umap1', 'umap2', 'umap3'])
-df_umap['label'] = df_sampled['Diabetes_binary'].values
-
-# Separate data by label
-df_0 = df_umap[df_umap['label'] == 0]
-df_1 = df_umap[df_umap['label'] == 1]
-
-trace0 = go.Scatter3d(
-    x=df_0['umap1'],
-    y=df_0['umap2'],
-    z=df_0['umap3'],
-    mode='markers',
-    name='Label 0',
-    marker=dict(size=10, symbol='circle'),
-    text=df_0.index  # optional: hover text
-)
-
-trace1 = go.Scatter3d(
-    x=df_1['umap1'],
-    y=df_1['umap2'],
-    z=df_1['umap3'],
-    mode='markers',
-    name='Label 1',
-    marker=dict(size=10, symbol='diamond'),
-    text=df_1.index
-)
-
-fig = go.Figure(data=[trace0, trace1])
-
-fig.update_layout(
-    title='UMAP Scatter Plot',
-    scene=dict(
-        xaxis_title='UMAP1',
-        yaxis_title='UMAP2',
-        zaxis_title='UMAP3'
-    ),
-    width=800,
-    height=600,
-    legend_title_text='Binary Label'
-)
-
-st.plotly_chart(fig)
